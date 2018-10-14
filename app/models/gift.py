54a1b585f80b6163dc5f4a67679f49b6a8b244c8
wangke <1052060838@qq.com>
2018-10-14 11:14:33 +0800

    fix: 首页最近赠送书籍展示完成

--- app/models/gift.py
+++ app/models/gift.py
@@ -5,10 +5,11 @@
 @contact: merpyzf@qq.com
 @software: PyCharm
 """
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String,desc
 from sqlalchemy.orm import relationship
 from app.models.base import Base
-
+from flask import current_app
+from app.spider.yushu_book import YuShuBook
 class Gift(Base):
     id = Column(Integer, primary_key=True)
     # 与User模型相关联
@@ -18,3 +19,24 @@ class Gift(Base):
     launched = Column(Boolean, default=False)
     # 不同的用户可能赠送出同一本书籍，因此isbn编号可以存在重复
     isbn = Column(String(15),nullable=False)
+    @property
+    def book(self):
+        yushu_book = YuShuBook()
+        yushu_book.search_by_isbn(self.isbn)
+        return yushu_book.first
+
+
+    # 论将recent方法修改成类方法的合理性，一个Gift对象实例仅表示一个礼物，而在这个仅表示一个礼物的对象中
+    # 又可以查询多条书籍的信息，显然不合适。违背面向对象的思想
+    @classmethod
+    def recent(self):
+        # 查询前30条最近赠送的书籍信息，先将查询到的所有赠送的数据（未赠出）排序，然后在取30条数据，这里
+        # 需要注意limit和order_by的顺序，应该先排序再使用limit从中取数据
+        # distinct用于实现去重操作，在去重前需要先根据能够区别图书的isbn编号先进行分组操作
+        # 链式调用,当调用all()或者first()方法才会去执行sql查询的操作
+        # desc()倒叙
+        return Gift.query.filter_by(launched = False).group_by(
+            Gift.isbn).order_by(
+            desc(Gift.create_time)).limit(
+            current_app.config['RECENT_BOOK_COUNT']).distinct().all()
+
